32a9252fb79c75652862f590d945eafbfff11981
varunananth <varunananthk@gmail.com>
2019-02-12 11:24:17 +0530

    [PATCH] Remove duplicates from uncommited list (#7)
    * [PATCH] Remove duplicates from uncommited list
    
    * remove duplicate objects

--- src/fetchGitStatus.js
+++ src/fetchGitStatus.js
@@ -1,7 +1,6 @@
 const {
   spawnSync
 } = require('child_process');
-
 const chalk = require('chalk');
 
 const {
@@ -20,36 +19,50 @@ function findError(error) {
   return error;
 }
 
+function removeDuplicateObjectsFromArray(finalList, nodeName) {
+  return finalList.filter((obj, index, arrayList) => {
+    return arrayList.map((mapObj) => mapObj[nodeName]).indexOf(obj[nodeName]) === index;
+  });
+}
+
+function removeDuplicateEntries(stagedFiles, unStagedFiles) {
+  // Need to check if the type is object as for showStatus=true we get an array of strings.
+  if(typeof stagedFiles[0] === 'object' || unStagedFiles[0] === 'object') {
+    return removeDuplicateObjectsFromArray([...stagedFiles, ...unStagedFiles], 'filename');
+  }
+  let finalList = new Set([...stagedFiles, ...unStagedFiles]);
+  return [...finalList];
+}
 function findFiles(cmd, formats, showStatus) {
-  
+
   let [bin, ...args] = cmd.split(' ');
   let changedFiles = spawnSync(bin, args);
   let error = '';
-  
+
   if (changedFiles.status) {
     error = findError(changedFiles.stderr);
     process.emit('onError', error);
     return;
   }
-  
+
   let files = changedFiles.stdout.toString().split('\n');
   files = files.slice(0,-1); // While splitting there is an empty string at last position.
-  
+
   if (formats) {
     files = filterFiles(files, formats);
   }
-  
+
   if(showStatus) {
     files = formatOutput(files);
   }
-  
+
   return files;
 }
 
 function fetchGitStatus(options) {
 
   return new Promise((resolve, reject) => {
-    
+
     let {
       baseBranch,
       diffFilter,
@@ -58,7 +71,7 @@ function fetchGitStatus(options) {
       showCommitted,
       showUnCommitted
     } = options;
-    
+
     if (!(showCommitted || showUnCommitted)) {
       return reject(chalk.keyword('orange')('Either showCommitted or showUnCommitted must be true.'));
     }
@@ -66,34 +79,34 @@ function fetchGitStatus(options) {
     let statusCmd = showStatus ? '--name-status' : '--name-only';
 
     let baseCmd = `git diff --relative ${statusCmd} --diff-filter=${diffFilter}`;
-    
+
     let commitedCmd = baseCmd.concat(` ${baseBranch}...HEAD`);
-    
+
     let stagedCmd = baseCmd.concat(' --staged');
-    
+
     let committedFiles, stagedFiles, unStagedFiles;
-    
+
     let fileList = {};
-    
+
     process.on('onError', (error) => {
       reject(chalk.red(error));
     });
 
     try {
-      
+
       if (showCommitted) {
         committedFiles = findFiles(commitedCmd, formats, showStatus);
         fileList.committedFiles = committedFiles;
-      } 
-      
+      }
+
       if (showUnCommitted) {
         stagedFiles = findFiles(stagedCmd, formats, showStatus);
         unStagedFiles = findFiles(baseCmd, formats, showStatus);
-        fileList.unCommittedFiles = [...stagedFiles, ...unStagedFiles];
+        fileList.unCommittedFiles = removeDuplicateEntries(stagedFiles, unStagedFiles);
       }
-  
+
       resolve(fileList);
-    
+
     } catch(err) {
       reject(chalk.red(err));
     }
